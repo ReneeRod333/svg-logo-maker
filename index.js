fe0e74c5c28d7a365698e3f5f474b0846affb209
Renee Rodriguez <poulin.reneemarie@gmail.com>
2024-06-24 00:28:29 -0400

    Implemented logic to index.js file. Added gitignore file. Added package.json file.

--- index.js
+++ index.js
@@ -1,3 +1,75 @@
-const { Circle, Triangle, Square} = require("./shapes");
-const circle = new Circle("red", "ABCDE", "#FF5733");
-console.log(circle.render());
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const { Triangle, Circle, Square } = require("./lib/shapes");
+
+// Questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Enter up to 3 text characters.",
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Enter a text color keyword or a hexadecimal number.",
+    },
+    {
+        type: "list",
+        name: "shapeType",
+        message: "Which shape will you be using?",
+        choices: ["Circle", "Square", "Triangle"],
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a shape color keyword or a hexadecimal number.",
+    }
+];
+
+// Function to write data to file
+function writeToFile(fileName, data) {
+    const file = fs.writeFileSync(path.join(process.cwd(), fileName), data);
+    return file;
+}
+
+async function init() {
+    let svgString = "";
+    const svgFile = "logo.svg";
+    const answers = await inquirer.prompt(questions);
+
+    const enteredShapeType = answers.shapeType;
+
+    const enteredShapeColor = answers.shapeColor;
+
+    const enteredTextColor = answers.textColor;
+
+    // Function if user enters more than 3 characters
+    let enteredText = "";
+    if (answers.text.length > 0 && answers.text.length < 4) {
+        enteredText = answers.text;
+    }
+    else {
+        console.log("Enter up to 3 characters only.");
+        return;
+    }
+
+    let enteredShape;
+    if (enteredShapeType === "Square" || enteredShapeType === "square") {
+        enteredShape = new Square(enteredShapeColor, enteredText, enteredTextColor);
+    }
+    else if (enteredShapeType === "Circle" || enteredShapeType === "circle") {
+        enteredShape = new Circle(enteredShapeColor, enteredText, enteredTextColor);
+    }
+    else if (enteredShapeType === "Triangle" || enteredShapeType === "triangle") {
+        enteredShape = new Triangle(enteredShapeColor, enteredText, enteredTextColor);
+    }
+
+    svgString = enteredShape.render();
+
+    console.log("Generated logo.svg");
+
+    writeToFile(svgFile, svgString);
+}
+init()
